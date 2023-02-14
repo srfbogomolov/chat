130c7b415dd51c6a7546ea82bed82fd05e080a12
Serafim Bogomolov <srf.bogomolov@protonmail.com>
2023-02-14 15:07:37 +0300

    Separate view from logic using template
    Moved the HTML code from inside `main.go` file to its own file `chat.html`
    in a `template` directory.
    
    Wrote own `templateHandler` struct type that is responsible for loading,
    compiling, and delivering template. That new type will take a filename
    string, compile the template once (using the `sync.Once` type), keep the
    reference to the compiled template, and the respond to HTTP requests.

--- main.go
+++ main.go
@@ -3,7 +3,23 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
 )
+
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, nil)
+}
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
